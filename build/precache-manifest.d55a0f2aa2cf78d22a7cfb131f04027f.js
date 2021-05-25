bbc92beac20463f3d11f2dff8e9db32e1bbdb91f
akpgaa <46481890+akpgaa@users.noreply.github.com>
2021-05-25 19:26:08 +0530

    build file

---
+++ build/precache-manifest.d55a0f2aa2cf78d22a7cfb131f04027f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf4ec057273a0dc23d62",
+    "url": "/static/css/main.2c447376.chunk.css"
+  },
+  {
+    "revision": "cf4ec057273a0dc23d62",
+    "url": "/static/js/main.cf4ec057.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "a31681a6fd740a153b413786b170be11",
+    "url": "/index.html"
+  }
+];
