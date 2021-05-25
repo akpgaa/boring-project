3aff8bb7a6d57433508d5b5aee8f805f39fb7ae7
akpgaa <46481890+akpgaa@users.noreply.github.com>
2021-05-25 19:08:35 +0530

    Portfolio app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import react from 'react'
+import Sidebar from './components/sidebar'
+import Introduction from './components/introduction'
+import About from './components/about'
+import Timeline from './components/timeline'
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-data:new Date()
-    }
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+         	<Sidebar></Sidebar>
+				<div id="colorlib-main">
+					<Introduction></Introduction>
+					<About></About>
+					<Timeline></Timeline>
+          	</div>
+      	</div>
+      </div>
+    );
   }
-  componentWillMount(){
-    var timer =  setInterval(() => {
-      this.setState({date:new Date()})
-    }, 1000);
-    return function cleanup() {
-        clearInterval(timer)
-    }
-
-  }
-  render(){
-    const {date}=this.state;
-    console.log(date);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Hi , Welcome !
-        {date &&<div>
-
-          <p> Time : {date.toLocaleTimeString()}</p>
-            <p> Date : {date.toLocaleDateString()}</p>
-        </div>
-            }
-      </header>
-    </div>
-  );
-}
 }
 
 export default App;
---
+++ src/components/timeline.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Associate developer at TRAXERP/Consortia22 <span>Nov,2020-present</span></h2>
+                        <p>I recently have joined the Associate developer at TRAXERP/Consortia22 in the DevOps team. My major works are Testing, evaluating new programs, and deploying into the Server</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>UG at UCE Tindivanam<span>2016-2020</span></h2>
+                        <p>I completed my B.E in Computer Science & Engineering from the University College of Engineering Tindinvanam</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2003-2016</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry,Maths &Computer Science.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
